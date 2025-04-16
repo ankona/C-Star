c3eaf3e3ee3463f1849860baeedc44be31432a42
Christopher McBride <3595025+ankona@users.noreply.github.com>
2025-04-16 15:00:53 -0400

    update and re-run tutorial notebooks

--- examples/cstar_example.py
+++ examples/cstar_example.py
@@ -1,21 +1,36 @@
 """
-This is a condensed version of the cstar_example_notebook.ipynb designed to be run in an interactive python session.
-For more details, see ../README.md or cstar_example_notebook.ipynb
+This is a condensed version of the `cstar_example_notebook.ipynb` designed
+to be run in an interactive python session.
+
+For more details, see ../README.md or `cstar_example_notebook.ipynb`
 """
 
 import cstar
 import os
 
-roms_marbl_case = cstar.Case.from_blueprint(
+roms_marbl_sim = cstar.Simulation.from_blueprint(
     blueprint="cstar_blueprint_yaml_test.yaml",
-    caseroot="roms_marbl_example_case/",
+    caseroot="roms_marbl_example/",
     start_date="20120103 12:00:00",
     end_date="20120103 18:00:00",
 )
 
 ## In a python session, execute:
-roms_marbl_case.setup()
-roms_marbl_case.build()
-roms_marbl_case.pre_run()
-#roms_marbl_case.run(account_key=os.environ.get('ACCOUNT_KEY')) #substituting your account key on any HPC system
-#roms_marbl_case.post_run()
+roms_marbl_sim.setup()
+roms_marbl_sim.build()
+roms_marbl_sim.pre_run()
+
+
+# Substitute your account key on any HPC system
+handler = roms_marbl_sim.run(account_key=os.environ.get("ACCOUNT_KEY"))
+
+# Use the handler to request status updates on the simulation
+handler.updates(0, confirm_indefinite=True)
+
+if handler.status != cstar.execution.handler.ExecutionStatus.RUNNING:
+    print(handler.status)
+
+if handler.status == cstar.execution.handler.ExecutionStatus.FAILED:
+    print("An error occurred. See:", handler.output_file)
+else:
+    roms_marbl_sim.post_run()
