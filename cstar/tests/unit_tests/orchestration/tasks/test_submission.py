76a0d6dd00fe96086c14683a44f40e478684d8db
Christopher McBride <3595025+ankona@users.noreply.github.com>
2025-10-15 17:56:40 -0400

    fix failure to run async

--- cstar/tests/unit_tests/orchestration/tasks/test_submission.py
+++ cstar/tests/unit_tests/orchestration/tasks/test_submission.py
@@ -1,3 +1,4 @@
+import asyncio
 from unittest import mock
 
 import pytest
@@ -14,4 +15,4 @@ def fake_sacct_result() -> str:
 @pytest.mark.usefixtures("prefect_server")
 def test_submission_flow(fake_sacct_result: str) -> None:
     with mock.patch("cstar.orchestration.xxxxx", new=fake_sacct_result):
-        submission.run_flow()
+        asyncio.run(submission.run_flow())
