b7bcdefc146a8a2139c6b16f6f17e797bcdcfab4
Christopher McBride <3595025+ankona@users.noreply.github.com>
2025-10-22 15:12:06 -0400

    Bug fix and organization of template handler

--- cstar/cli/template/actions/template.py
+++ cstar/cli/template/actions/template.py
@@ -1,6 +1,5 @@
 import argparse
 import json
-import os
 import textwrap
 import typing as t
 from pathlib import Path
@@ -10,26 +9,113 @@ from cstar.orchestration.models import RomsMarblBlueprint, Workplan
 from cstar.system.manager import cstar_sysmgr as system_mgr
 
 
-def handle(ns: argparse.Namespace) -> None:
-    """The action handler for the template-create action.
+def check_or_create_path(ns: argparse.Namespace) -> None:
+    """Verify a valid directory was supplied.
 
-    Triggers creation of a sample template.
+    If necessary, prompts the user for permission to create when interactive
+    mode is enabled.
+
+    Parameters
+    ----------
+    ns : argparse.Namespace
+        User inputs parsed by the CLI
     """
-    interactive: bool = os.getenv("CSTAR_INTERACTIVE", "1") == "1"
-    template = ns.type
     path: Path = ns.path
+    interactive: bool = ns.interactive
 
     if path and not path.exists() and interactive:
         do_create = input("The directory does not exist. Create it? (yes/no): ")
         if "y" in do_create.lower():
-            path.mkdir(parents=True, exist_ok=False)
+            path.mkdir(parents=True, exist_ok=True)
         else:
             raise ValueError("Unable to create template without valid directory")
 
     if path and not path.exists():
         raise ValueError(f"The specified directory does not exist: {path}")
 
+
+def get_inline_output(
+    template_type: t.Literal["workplan", "blueprint"],
+    schema: str,
+    template: str,
+) -> str:
+    """Generate the schema and template output formatted for stdout.
+
+    Parameters
+    ----------
+    template_type : str
+        The template type that will be generated.
+    schema : str
+        The schema content for the selected model.
+    template : str
+        The template content for the selected model.
+
+    Returns
+    -------
+    str
+        Formatted output
+    """
     delimiter = "*" * 80
+
+    s_header = textwrap.dedent(
+        f"""\
+        {delimiter}
+        * {template_type.capitalize()} schema
+        {delimiter}
+        """
+    )
+    t_header = textwrap.dedent(
+        f"""\
+        {delimiter}
+        * {template_type.capitalize()} template
+        {delimiter}
+        """
+    )
+    return f"{s_header}{schema}\n\n{t_header}{template}"
+
+
+def replace_schema_directive(template: str, schema_path: Path | None) -> str:
+    """Replace the schema directive in the original template with a path
+    to a newly generated schema file.
+
+    Parameters
+    ----------
+    template : str
+        The template content to augment
+    schema_path : Path or None
+        The path to a schema file
+
+    Returns
+    -------
+    str
+        The modified template content
+    """
+    if schema_path:
+        schema_ref = f"# yaml-language-server: $schema=file://{schema_path}"
+    else:
+        schema_ref = "# yaml-language-server: $schema=<schema-uri>"
+
+    template_lines = template.split("\n")
+    template_lines[0] = schema_ref
+
+    return "\n".join(template_lines)
+
+
+def handle(ns: argparse.Namespace) -> None:
+    """The action handler for the template-create action.
+
+    Triggers creation of a sample template.
+
+    Parameters
+    ----------
+    ns : argparse.Namespace
+        User inputs parsed by the CLI
+    """
+    check_or_create_path(ns)
+
+    template = ns.type
+    path: Path | None = ns.path
+
     tpl_name = f"{template}.yaml"
     schema_name = f"{template}-schema.yaml"
     tpl_source_path = system_mgr.environment.template_root / tpl_name
@@ -41,7 +127,6 @@ def handle(ns: argparse.Namespace) -> None:
     template_path: Path | None = None
 
     if path is not None:
-        path.mkdir(parents=True, exist_ok=True)
         template_path = path / tpl_name
         schema_path = path / schema_name
 
@@ -50,40 +135,22 @@ def handle(ns: argparse.Namespace) -> None:
     else:
         schema = json.dumps(RomsMarblBlueprint.model_json_schema(), indent=4)
 
-    if schema_path:
-        schema_ref = f"# yaml-language-server: $schema=file://{schema_path}"
-    else:
-        schema_ref = "# yaml-language-server: $schema=<schema-uri>"
-
-    tpl = tpl_source_path.read_text(encoding="utf-8")
-    template_lines = tpl.split("\n")
-    template_lines[0] = schema_ref
-    tpl = "\n".join(template_lines)
+    original_template = tpl_source_path.read_text(encoding="utf-8")
+    template_content = replace_schema_directive(original_template, schema_path)
 
     if template_path and schema_path:
         schema_path.write_text(schema, encoding="utf-8")
-        template_path.write_text(tpl, encoding="utf-8")
+        template_path.write_text(template_content, encoding="utf-8")
 
         message = textwrap.dedent(
             f"""\
-            {template} schema written to: {schema_path}
-            {template} template written to: {template_path}
+            {template.capitalize()} schema written to: {schema_path}
+            {template.capitalize()} template written to: {template_path}
             """
         )
     else:
-        message = textwrap.dedent(
-            f"""\
-            {delimiter}
-            * {template} schema
-            {delimiter}
-            {schema}
+        message = get_inline_output(template, schema, template_content)
 
-            {delimiter}
-            * {template} template
-            {delimiter}
-            {tpl}
-            """
-        )
     print(message)
 
 
@@ -109,6 +176,7 @@ def create_action() -> RegistryResult:
                 "the template is written to stdout."
             ),
             required=False,
+            default=None,
             action=PathConverterAction,
         )
         parser.add_argument(
