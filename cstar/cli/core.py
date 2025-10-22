fce523511853416d9ab49ab3f0f6583c9b62c298
Christopher McBride <3595025+ankona@users.noreply.github.com>
2025-10-22 15:11:26 -0400

    add interactive flag as default parameter

--- cstar/cli/core.py
+++ cstar/cli/core.py
@@ -1,3 +1,4 @@
+import os
 import typing as t
 from argparse import Action, ArgumentParser, Namespace, _SubParsersAction
 from collections import defaultdict
@@ -46,6 +47,9 @@ def build_parser() -> tuple[ArgumentParser, _SubParsersAction]:
         required=True,
         dest="command",
     )
+
+    interactive: bool = os.getenv("CSTAR_INTERACTIVE", "1") == "1"
+    parser.set_defaults(interactive=interactive)
 
     return parser, subparsers
 
