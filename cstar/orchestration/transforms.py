b1720e7e44ef91e4f0c3309d437fdcaa3d34b064
Christopher McBride <3595025+ankona@users.noreply.github.com>
2025-11-26 15:03:53 -0500

    relocate deep_merge to utils module

--- cstar/orchestration/transforms.py
+++ cstar/orchestration/transforms.py
@@ -6,7 +6,7 @@ from pydantic import HttpUrl
 
 from cstar.orchestration.models import RomsMarblBlueprint, Step
 from cstar.orchestration.serialization import deserialize
-from cstar.orchestration.utils import slugify
+from cstar.orchestration.utils import deep_merge, slugify
 
 JOIN_DIR_NAME: t.Literal["JOINED_OUTPUT"] = "JOINED_OUTPUT"
 
@@ -109,29 +109,6 @@ def get_time_slices(
         time_slices[-1] = (time_slices[-1][0], end_date)
 
     return time_slices
-
-
-def deep_merge(d1: dict[str, t.Any], d2: dict[str, t.Any]) -> dict[str, t.Any]:
-    """Deep merge two dictionaries.
-
-    Parameters
-    ----------
-    d1 : dict[str, t.Any]
-        The first dictionary.
-    d2 : dict[str, t.Any]
-        The second dictionary.
-
-    Returns
-    -------
-    dict[str, t.Any]
-        The merged dictionaries.
-    """
-    for k, v in d2.items():
-        if isinstance(v, dict):
-            d1[k] = deep_merge(d1.get(k, {}), v)
-        else:
-            d1[k] = v
-    return d1
 
 
 class RomsMarblTimeSplitter(Splitter):
--- cstar/orchestration/utils.py
+++ cstar/orchestration/utils.py
@@ -1,4 +1,5 @@
 import re
+import typing as t
 
 
 def slugify(source: str) -> str:
@@ -19,3 +20,26 @@ def slugify(source: str) -> str:
 
     alphanumeric = re.sub(r"\W", "", source.casefold())
     return re.sub(r"\s+", "-", alphanumeric)
+
+
+def deep_merge(d1: dict[str, t.Any], d2: dict[str, t.Any]) -> dict[str, t.Any]:
+    """Deep merge two dictionaries.
+
+    Parameters
+    ----------
+    d1 : dict[str, t.Any]
+        The first dictionary.
+    d2 : dict[str, t.Any]
+        The second dictionary.
+
+    Returns
+    -------
+    dict[str, t.Any]
+        The merged dictionaries.
+    """
+    for k, v in d2.items():
+        if isinstance(v, dict):
+            d1[k] = deep_merge(d1.get(k, {}), v)
+        else:
+            d1[k] = v
+    return d1
